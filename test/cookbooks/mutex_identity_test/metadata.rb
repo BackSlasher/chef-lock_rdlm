90568071ead8a9b575ae83b288bf82278b48dc1e
Nitz <nitz.raz@gmail.com>
2015-11-26 13:00:14 +0200

    Added an initial kitchen test for server recipe

---
+++ test/cookbooks/mutex_identity_test/metadata.rb
@@ -0,0 +1,9 @@
+name             'mutex_identity_test'
+maintainer       'Backslasher'
+maintainer_email 'nitz.raz@gmail.com'
+license          'GPL v2'
+description      'Wrapper cookbook to test mutex_identity'
+long_description 'Wrapper cookbook to test mutex_identity'
+version          '0.1.0'
+
+depends          'mutex_identity'
---
+++ test/cookbooks/mutex_identity_test/recipes/server.rb
@@ -0,0 +1,25 @@
+# Tests the server
+
+execute 'service rdlm-daemon status' # Should exit with 0
+
+bash_script 'test-rdlm' do
+  code <<-EOT
+  set -e
+  PORT=#{node['mutex_identity']['port']}
+  # Get lock
+  RAW_LOCK=$(curl -sI -f -d '{"title": "client", "wait": 5, "lifetime": 300}' http://localhost:7305/locks/testy)
+  LOCK_URL=$(echo "$RAW_LOCK" | perl -ne 'print $1 if /^Location: (.*)$/')
+  # Fail to get another lock
+  if curl -sI -f -d '{"title": "different client", "wait": 5, "lifetime": 300}' http://localhost:7305/locks/testy; then
+    echo 'got another lock. Bad'
+    exit 1
+  fi
+  # Delete lock
+  curl -f -x DELETE "$LOCK_URL"
+  # Fail to delete again
+  if curl -f -x DELETE "$LOCK_URL"; then
+    echo 'managed to delete same lock twice. Bad'
+    exit 2
+  fi
+  EOT
+end
