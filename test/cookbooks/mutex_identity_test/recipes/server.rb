25abe192ed116ea4a4e3adface67cb6c9f798e66
Nitz <nitz.raz@gmail.com>
2015-11-26 16:56:43 +0200

    Fixed some test recipes

--- test/cookbooks/mutex_identity_test/recipes/server.rb
+++ test/cookbooks/mutex_identity_test/recipes/server.rb
@@ -1,22 +1,27 @@
 # Tests the server
 
-execute 'service rdlm-daemon status' # Should exit with 0
+execute 'service rdlm status' # Should exit with 0
 
 bash 'test-rdlm' do
   code <<-EOT
   set -e
   PORT=#{node['mutex_identity']['port']}
   # Get lock
-  RAW_LOCK=$(curl -sI -f -d '{"title": "client", "wait": 5, "lifetime": 300}' http://localhost:7305/locks/testy)
+  echo get lock
+  RAW_LOCK=$(curl -sI -f -d '{"title": "client", "wait": 5, "lifetime": 300}' http://localhost:$PORT/locks/testy)
+  echo extract lock url
   LOCK_URL=$(echo "$RAW_LOCK" | perl -ne 'print $1 if /^Location: (.*)$/')
   # Fail to get another lock
-  if curl -sI -f -d '{"title": "different client", "wait": 5, "lifetime": 300}' http://localhost:7305/locks/testy; then
+  echo failing another lock
+  if curl -sI -f -d '{"title": "different client", "wait": 5, "lifetime": 300}' http://localhost:$PORT/locks/testy; then
     echo 'got another lock. Bad'
     exit 1
   fi
   # Delete lock
+  echo delete lock
   curl -f -x DELETE "$LOCK_URL"
   # Fail to delete again
+  echo failing to re-delete lock
   if curl -f -x DELETE "$LOCK_URL"; then
     echo 'managed to delete same lock twice. Bad'
     exit 2
