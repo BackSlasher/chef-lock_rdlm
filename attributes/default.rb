714776a98651b8a19f8dc43b8222d3a212ba5f67
Nitz <nitz.raz@gmail.com>
2015-11-26 14:46:32 +0200

    Created attributes

---
+++ attributes/default.rb
@@ -0,0 +1,7 @@
+# Sets default values to attributes
+
+default['mutex_identity']['scheme'] = 'http'
+default['mutex_identity']['hostname'] = nil
+default['mutex_identity']['port'] = 7305
+default['mutex_identity']['wait'] = 5
+default['mutex_identity']['lifetime'] = 300
