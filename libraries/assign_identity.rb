932577b77558e9dd7dfccc4cbc3f6cf948db7e4e
Nitz <nitz.raz@gmail.com>
2015-12-01 11:37:30 +0200

    Major lib organization
    Not tested yet

--- libraries/assign_identity.rb
+++ libraries/assign_identity.rb
@@ -7,25 +7,53 @@
 # Assign a unique value to a node's attribute
 
 module MutexRDLM
-  def assign_identity(node,assignment_path,range,*additional_config)
-    # parse config
-    mutex_url = additional_config.delete(:mutex_url)
-    mutex_wait = additional_config.delete(:mutex_wait)
-    mutex_lifetime = additional_config.delete(:mutex_lifetime)
-    raise "Unknown options passed: #{additional_config.keys}" if additional_config.keys.any?
+  # get value from node
+  def _get_value(node,assignment_path)
+    node.attributes.merged_attributes(*assignment_path)
+  end
 
-    unless mutex_url then
-      url_blocks = node['mutex_rdlm'].values_at(:scheme,:hostname,:port)
-      raise 'cannot deduce mutex server. Please specify one using attributes or function arguments' if url_blocks.any?{|v|!v}
-      mutex_url = "#{url_blocks[0]}://#{url_blocks[1]}:#{url_blocks[2]}"
+  # Set a value in a vivid hash/array, recursively
+  def _vivid_set(vivid,assignment_path,value)
+    if assignment_path.length==1
+      vivid[assignment_path.first]=value
+    else
+      _vivid_set(vivid[assignment_path.first],assignment_path.drop(1))
     end
+  end
 
-    mutex_wait||=node['mutex_rdlm']['wait']
-    mutex_lifetime=node['mutex_lifetime']['lifetime']
+  # set value in node's normal
+  def _set_normal(node,assignment_path,value)
+    _vivid_set(node.normal,assignment_path,value)
+  end
 
-    # Start working
+  # set value in node's server
+  def _set_server(node,assignment_path,value)
+    s_node = Chef::Node.load(node.name)
+    _set_normal(s_node,assignment_path,value)
+    s.save
+  end
+
+  def assign_identity(node,assignment_path,range,*additional_config)
     current_value =  _get_value(node,assignment_path)
     return current_value if current_value # abort if value is already set
-    mutex_name = assignment_path.map{|p|p.gsub(/\W|_/,'')}.join
+    # We have to invent a new value
+    with_mutex(node,assignment_path.join,*additional_config) do
+      # Find existing values
+      node_names = Chef::Node.list.keys
+      existing_values = node_names.map do |name|
+        n = Chef::Node.load(name)
+        _get_value(n,assignment_path)
+      end
+      remaining_range = range - existing_values
+      # Choose my value
+      if remaining_range.empty?
+        raise 'Identity range is exhausted' #TODO use custom exception
+      else
+        my_value = remaining_range.first
+        _set_normal(node,assignment_path,my_value)
+        _set_server(node,assignment_path,my_value)
+        return my_value
+      end
+    end
   end
 end
--- libraries/mutex.rb
+++ libraries/mutex.rb
@@ -9,16 +9,69 @@
 module MutexRDLM
 
   # Locks a mutex. Returns a mutex object (used to release it) if successful
-  def mutex_lock(server,mutex_name,wait,lifetime)
+  def mutex_lock(server,mutex_name,client_name,wait,lifetime)
     require 'net/http'
     uri = URI(server)
-    resp = Net::HTTP.new(uri.host).get(uri)
-    #TODO throw if not 2XX
-    obj = resp['Location']
-    return obj
+    uri.path="/#{mutex_name}"
+    require 'json'
+    data = {title: client_name, wait: wait, lifetime: lifetime }.to_json
+    resp = Net::HTTP.new(uri.host,uri.port).post(uri,data) #TODO is it sending ok?
+    if resp.code == '201'
+      obj = resp['Location']
+      return obj
+    elsif resp.code == '408'
+      raise 'Lock request timed out' #TODO use my exception
+    elsif resp.code == '409'
+      raise 'Lock request deleted by admin' #TODO use my exception
+    elsif resp.code == '400'
+      raise 'Bad lock request' #TODO use my exception
+    else
+      raise "Unknown response #{resp}" #TODO use my exception
+    end
   end
 
   def mutex_release(mutex_object)
+    require 'net/http'
+    uri = URI(mutex_object)
+    resp = Net::HTTP.new(uri.host,uri.port).get(uri)
+    if resp.code == '204'
+      return
+    elsif resp.code == '404'
+      raise 'Lock not found' #TODO use my exception
+    else
+      raise "Unknown response #{resp}"
+    end
+  end
 
+  def _normalize_name(source)
+    source.gsub(/\W|_/,'')
+  end
+
+  def with_mutex(node,mutex_resource,*additional_config)
+    # parse config
+    mutex_url = additional_config.delete(:mutex_url)
+    mutex_wait = additional_config.delete(:mutex_wait)
+    mutex_lifetime = additional_config.delete(:mutex_lifetime)
+    raise "Unknown options passed: #{additional_config.keys}" if additional_config.keys.any? #TODO use special exception
+
+    unless mutex_url then
+      url_blocks = node['mutex_rdlm'].values_at(:scheme,:hostname,:port)
+      raise 'cannot deduce mutex server. Please specify one using attributes or function arguments' if url_blocks.any?{|v|!v} #TODO use my exception
+      mutex_url = "#{url_blocks[0]}://#{url_blocks[1]}:#{url_blocks[2]}"
+    end
+
+    mutex_wait||=node['mutex_rdlm']['wait']
+    mutex_lifetime=node['mutex_lifetime']['lifetime']
+
+    # Start working
+    mutex=mutex_lock(mutex_url,
+               _normalize_name(mutex_resource),
+               _normalize_name(node.name),
+               mutex_wait,mutex_lifetime)
+    begin
+      yield if block_given?
+    ensure # Always delete mutex
+      mutex_release(mutex)
+    end
   end
 end
