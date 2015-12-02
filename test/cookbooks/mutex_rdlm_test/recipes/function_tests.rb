7d9f7477009130be8baf2b08a4bf7b434c3c9642
Nitz <nitz.raz@gmail.com>
2015-12-02 16:05:46 +0200

    Added test for mutex management

--- test/cookbooks/mutex_rdlm_test/recipes/function_tests.rb
+++ test/cookbooks/mutex_rdlm_test/recipes/function_tests.rb
@@ -1,5 +1,15 @@
 # Executes my functions on the RDLM server
 
+ruby_block 'create-and-delete' do
+  block do
+    mutex_name='blob'
+    mu = MutexRDLM.mutex_lock("http://localhost:#{node['mutex_rdlm']['port']}",mutex_name,'nonya',5,300)
+    raise 'Mutex missing' unless MutexRDLM.mutex_check(mu)
+    MutexRDLM.mutex_release(mu)
+    raise 'Mutex lingers' if MutexRDLM.mutex_check(mu)
+  end
+end
+
 ruby_block 'test-with-mutex' do
   block do
     node.default['mutex_rdlm']['hostname']='localhost'
