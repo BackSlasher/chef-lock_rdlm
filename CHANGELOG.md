d5f9fa37d276f8e6f4be0fc1feb172367fb1bc78
Nitz <nitz.raz@gmail.com>
2015-12-03 16:55:38 +0200

    Bumped version to 0.1.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,5 @@
-# 0.1.0
+# 0.1.1
+- [backslasher] Updated README
 
+# 0.1.0
 Initial release of mutex_rdlm
--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'nitz.raz@gmail.com'
 license          'GPL v2'
 description      'Installs/Configures mutex_rdlm'
 long_description 'Installs/Configures mutex_rdlm'
-version          '0.1.0'
+version          '0.1.1'
 
 depends 'poise-python', '~> 1.1'
